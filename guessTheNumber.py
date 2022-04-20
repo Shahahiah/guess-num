917519e84b86b2439a04cfb68c0112305f2dfbb4
Jason Cheung <yongadorjechang@gmail.com>
2022-04-21 06:16:42 +0800

    Update file

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -7,15 +7,15 @@ print('I am thinking of a number between 1 and 20.')
 for guessesTaken in range(1, 7):
 	print('Take a guess.')
 	guess = int(input())
-	
+
 	if guess < secretNumber:
 		print('Your guess is too low.')
 	elif guess > secretNumber:
-		print:('Your guess is too high.')
+		print('Your guess is too high.')
 	else:
 		break	#This condition is the correct guess!
 
 if guess == secretNumber:
-	print('Good job! You guessed my number in ' + str(guessesTaken) + 'guesses!')
+	print('Good job! You guessed my number in ' + str(guessesTaken) + ' guesses!')
 else:
 	print('Nope. The number I was thinking of was ' + str(secretNumber))
